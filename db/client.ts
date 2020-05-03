113f76fc814f273ed7361f968b8bc98526ed8024
Dominik Bláha <dominik.blaha@avast.com>
2020-05-03 12:23:50 +0200

    Different way to connect heroku database

--- db/client.ts
+++ db/client.ts
@@ -1,7 +1,4 @@
 import { Client } from "https://deno.land/x/postgres/mod.ts"
 const env = Deno.env()
 
-export const client = new Client({
-  connectionString: env.DATABASE_URL,
-  ssl: true
-})
+export const client = new Client(env.DATABASE_URL)
