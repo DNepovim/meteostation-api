048e4779c5f3c0ceb83ffe1c52f31dad29e7a132
Dominik Bláha <dominik.blaha@avast.com>
2020-05-03 17:00:52 +0200

    Fix config

--- config.ts
+++ config.ts
@@ -1,9 +1,7 @@
-const env = Deno.env()
-
-export const APP_HOST = env.APP_HOST || "127.0.0.1"
-export const APP_PORT = env.APP_PORT || 4000
-export const DB_USER = env.DB_USER || "meteo"
-export const DB_DATABASE = env.DB_DATABASE || "meteo"
-export const DB_HOST = env.DB_HOST || "localhost"
-export const DB_PORT = env.DB_PORT || "5445"
-export const DB_PASSWORD = env.DB_PASSWORD || "meteo"
+export const APP_HOST = Deno.env("APP_HOST") || "127.0.0.1"
+export const APP_PORT = Deno.env("APP_PORT") || 4000
+export const DB_USER = Deno.env("DB_USER") || "meteo"
+export const DB_DATABASE = Deno.env("DB_DATABASE") || "meteo"
+export const DB_HOST = Deno.env("DB_HOST") || "localhost"
+export const DB_PORT = Deno.env("DB_PORT") || "5445"
+export const DB_PASSWORD = Deno.env("DB_PASSWORD") || "meteo"
